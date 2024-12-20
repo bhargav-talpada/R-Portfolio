935b1b9da771c78a90afab0bb77bf537bc7dc79e
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-12-20 18:00:03 +0530

    resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/Bhargav cv.pdf",
+      href: "/Bhargav.pdf",
       style: "rounded-br-md",
       download: true,
     },
